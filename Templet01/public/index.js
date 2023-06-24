8d73a28a14fd770a0c7b9f949c683cfad45f0341
Hassan Hossam <egoker1234@gmail.com>
2023-06-24 11:39:57 +0300

    All folders

--- Templet01/public/index.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("icon-click").onclick=function(){
-    if (document.getElementById("box-nav").style.display=="block") {
-        document.getElementById("box-nav").style.display="none";
-    }else{
-        document.getElementById("box-nav").style.display="block";
-    }
-   
-}
--- Templet03/public/main.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("nav-bar").onclick=function(){
-    
-    if ( document.getElementById("box-elements").style.display=="flex"){
-        document.getElementById("box-elements").style.display="none";
-    }else{
-        document.getElementById("box-elements").style.display="flex";
-    }}
-
