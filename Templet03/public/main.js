1d66a5dff04570ae91d7c0b5c2d744e8ed71dc6e
Hassan Hossam <egoker1234@gmail.com>
2023-06-24 11:50:56 +0300

    All Folders

---
+++ Templet03/public/main.js
@@ -0,0 +1,9 @@
+
+document.getElementById("nav-bar").onclick=function(){
+    
+    if ( document.getElementById("box-elements").style.display=="flex"){
+        document.getElementById("box-elements").style.display="none";
+    }else{
+        document.getElementById("box-elements").style.display="flex";
+    }}
+
